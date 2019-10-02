2e8e6706a3110e0bdaf0a2dadbf3b682c953ed62
alexanderlakiza <alelakiza@yandex.ru>
2019-10-02 18:39:36 +0300

    Реализованы функции encrypt_vigenere и decrypt_vigenere

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -13,18 +13,20 @@ def encrypt_vigenere(plaintext, keyword):
     for ch in plaintext:
         if keyword[i].isupper():
             shift = ord(keyword[i]) - 65
-        else:
+        elif keyword[i].islower():
             shift = ord(keyword[i]) - 97
         if ch.isupper():
             ciphertext += chr((ord(ch) + shift - 65) % 26 + 65)
-        else:
+        elif ch.islower():
             ciphertext += chr((ord(ch) + shift - 97) % 26 + 97)
         i += 1
 
     return ciphertext
+
+
 # зашифровали
 
-def decrypt_vigenere(ciphertext):
+def decrypt_vigenere(ciphertext, keyword):
     """
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
@@ -33,5 +35,19 @@ def decrypt_vigenere(ciphertext):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # put ur code here
+    plaintext = ''
+    keyword *= len(ciphertext) // len(keyword) + 1
+    i = 0
+    for ch in ciphertext:
+        if keyword[i].isupper():
+            shift = ord(keyword[i]) - 65
+        elif keyword[i].islower():
+            shift = ord(keyword[i]) - 97
+        if ch.isupper():
+            plaintext += chr((ord(ch) - shift - 65) % 26 + 65)
+        elif ch.islower():
+            plaintext += chr((ord(ch) - shift - 97) % 26 + 97)
+        i += 1
+
     return plaintext
+# расшифровали
